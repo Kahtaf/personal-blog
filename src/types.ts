560949a600e83befbacdd762ad82de3df20d584e
Kahtaf Alam <kahtaf@gmail.com>
2026-07-06 18:04:59 -0400

    Fix SEO audit findings

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,11 @@
 export type Site = {
   NAME: string;
   EMAIL: string;
+  DESCRIPTION: string;
+  JOB_TITLE: string;
+  LOCATION: string;
+  IMAGE: string;
+  KNOWS_ABOUT: string[];
   NUM_POSTS_ON_HOMEPAGE: number;
   NUM_WORKS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
